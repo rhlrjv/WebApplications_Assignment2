287260a1b0db9695dbfcb4045caa01fd0a93a7c5
shambavi92 <shambavi92@gmail.com>
2014-03-26 18:08:50 +0800

    UserCreate.php implemented (main.js changed)

--- php/userCreate.php
+++ php/userCreate.php
@@ -1,17 +1,18 @@
 <?php
 	/* ------------------------------------------------
-	 login.php: 
+	 userCreate.php: 
 
-		login to the application if valid credentials 
-		are supplied, otherwise the user is logged out.
-
-		$_SESSION['is_logged_in']=="true" on success
-		$_SESSION['is_logged_in']=="false" on failure
+		sign up as a new user if username is unique
+		and all other form fieldds are valid,
+		otherwise the user is requested to refill/retry.
 
 	Parameters: 
 
 		username - required, non-empty string, the users user name
 		password - required, non-empty string, the users password
+		re-enter password - required, non-empty string, same as the users password
+		email - required, non-empty string, the users email
+		dob - required, non-empty string, the users date of birth
 
 	Returns: 
 		{ status: "ok" } on success
@@ -19,54 +20,113 @@
 	
 	------------------------------------------------ */
 
-	session_save_path("sess");
-	session_start(); 
+	require 'config.inc';
+	require 'Model.php';
+	
 	header('Content-Type: application/json');
 
 	// ------------------------------------------------
 	// Default reply
 	// ------------------------------------------------
 	$reply=array();
-	$reply['status']='ok';
+	$reply = array();
 
 	// ------------------------------------------------
 	// Default resulting state
 	// ------------------------------------------------
 	$_SESSION["is_logged_in"]="false";
 
+	$reply['status'] ='error';
+
+	$errors['msg'] = array();
+	$errors['username'] = false ;//no error
+	$errors['password'] = false ;//no error
+	$errors['reenterpassword'] = false ;//no error
+	$errors['email'] = false ;//no error
+	$errors['dob'] = false ;//no error
+
+	// ------------------------------------------------
+	// retreive assessed data
+	// ------------------------------------------------
+
+	if(!isset($_REQUEST['data']) || !isset($_REQUEST['requestType']) )
+		goto leave;
+
+	$data = json_decode($_REQUEST['data'],true);
+	$requestType = $_REQUEST["requestType"];
+	$requestUsername = $data["UserName"];
+	$requestPassword = $data["Password"];
+	$requestReenterpassword = $data["reEnterPassword"];
+	$requestEmail = $data["email"];
+	$requestDob = $data["dob"];
+
 	// ------------------------------------------------
 	// Check parameters
 	// ------------------------------------------------
-	if(empty($_REQUEST['user'])){
-		$reply['status']='user must be supplied';
-	}
-	if(empty($_REQUEST['password'])){
-		// Below is conceptually wrong! You should not return HTML.
-		// These scripts should only return data. It is not concerned
-		// with user interface.
-		$reply['status'] = $reply['status'] . '<br/> password must be supplied';
+	if(empty($requestUsername) || empty($requestPassword) || empty($requestReenterpassword) || empty($requestEmail) || empty($requestDob)){
+		$errors['msg'][] ='Incomplete form fields';
+
+		if(empty($requestUsername))
+			$errors['username'] = true;
+		if(empty($requestPassword))
+			$errors['password'] = true;
+		if(empty($requestReenterpassword))
+			$errors['reenterpassword'] = true;
+		if(empty($requestEmail))
+			$errors['email'] = true;
+		if(empty($requestDob))
+			$errors['dob'] = true;
 	}
 
-	if($reply['status']!='ok'){
-		goto leave;
+	if($requestPassword != $requestReenterpassword)
+	{
+		$errors['msg'][] ='Passwords dont match';
+		$errors['password'] = true;
+		$errors['reenterpassword'] = true;
 	}
+
+	if (preg_match('/[\'^£$%&*()}{@#~?><>,|=+¬-]/', $requestUsername))
+	{
+	    $errors['msg'][] ='Username cannot have special characters';
+	    $errors['username'] = true;
+	    $errors['reenterpassword'] = true;
+	}
+
+	if (preg_match('/[\'^£$%&*()}{@#~?><>,|=+¬-]/', $requestPassword))
+	{
+	    $errors['msg'][] ='Password cannot have special characters';
+	    $errors['password'] = true;
+	}
+
+	if($GLOBALS['userobj']->duplicateUsername($_SESSION['dbconn'], $requestUsername))
+	{
+		$errors['msg'][] ='Username already exists';
+		$errors['username'] = true;
+	}
+
+	if (count($errors['msg']) > 0)
+		goto leave;
 
 	// ------------------------------------------------
 	// Perform operation 
 	// ------------------------------------------------
 
 	// Check the database...SELECT FROM ... $1 ... $2
-	if($_REQUEST['user']!="arnold" || $_REQUEST['password']!="spiderman"){
-		$reply['status']="invalid login";
-		$_SESSION["is_logged_in"]="true";
+	if($GLOBALS['userobj']->signup($_SESSION['dbconn'], $requestUsername, $requestPassword, $requestEmail, $requestDob) == true)
+	{
+		$reply['status']= 'ok';
 		goto leave;
-	} else {
-		$_SESSION["is_logged_in"]="true";
+	} 
+
+	else 
+	{
+		$errors['msg'][] ='Incorrect sign up details';
 	}
 
 	// ------------------------------------------------
 	// Send reply 
 	// ------------------------------------------------
 	leave:
+	$reply['errors'] = $errors;
 	print json_encode($reply);
 ?>
